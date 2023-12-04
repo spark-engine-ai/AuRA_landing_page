14e8a6f265c1b36e752fd7f93b5dc35609122cc7
yvngjordi <61796788+yvngjordi@users.noreply.github.com>
2023-12-04 13:23:47 -0800

    Update main.tsx

--- components/main.tsx
+++ components/main.tsx
@@ -8,8 +8,10 @@ import GridSection from './gridsection';
 const Main: React.FC = () => {
   return (
     <div>
-      <VideoBackground />
-      <div className="flex flex-col items-center justify-center h-screen text-white" style={{zIndex:999}}>
+      <div style={{zIndex:'-1'}}>
+        <VideoBackground />
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen text-white" style={{zIndex:'999'}}>
         <Logo />
         <h2 className="text-3xl font-bold mt-2">AuRA</h2>
         <p className="mt-2">Automated React Architecture</p>
