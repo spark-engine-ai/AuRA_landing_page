4f95079b52d671dc2cfdc11d01ec8e9dae90f9b2
yvngjordi <61796788+yvngjordi@users.noreply.github.com>
2023-12-04 13:33:39 -0800

    Update videobackground.tsx

--- components/videobackground.tsx
+++ components/videobackground.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 const VideoBackground: React.FC = () => {
   return (
-    <div className="video-background" style={{zIndex:'-1'}}>
-      <video autoPlay muted loop className="w-full h-full object-cover fixed opacity-20">
+    <div className="video-background">
+      <video autoPlay muted loop className="w-full h-full fixed opacity-20" style={{zIndex:'-1'}}>
         <source src="bgvideo.mp4" type="video/mp4" />
       </video>
     </div>
