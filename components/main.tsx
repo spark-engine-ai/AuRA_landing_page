24d12a904c1228dfdfa3c7026fad79e82b9e7502
yvngjordi <61796788+yvngjordi@users.noreply.github.com>
2023-12-04 13:18:13 -0800

    Update main.tsx

--- components/main.tsx
+++ components/main.tsx
@@ -9,7 +9,7 @@ const Main: React.FC = () => {
   return (
     <div>
       <VideoBackground />
-      <div className="flex flex-col items-center justify-center h-screen text-white">
+      <div className="flex flex-col items-center justify-center h-screen text-white" style={{zIndex:999}}>
         <Logo />
         <h2 className="text-3xl font-bold mt-2">AuRA</h2>
         <p className="mt-2">Automated React Architecture</p>
